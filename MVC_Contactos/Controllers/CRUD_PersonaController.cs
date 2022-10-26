b493be1b0230a1b42321ecc2e9569e92567ebc60
Andrés Gómez <andresgomez040106@gmail.com>
2022-10-26 12:29:21 -0500

    Se agrega información IndexEmpleadoCRUD

---
+++ MVC_Contactos/Controllers/CRUD_PersonaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Contactos.Controllers
+{
+    public class CRUD_PersonaController : Controller
+    {
+        // GET: CRUD_Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
